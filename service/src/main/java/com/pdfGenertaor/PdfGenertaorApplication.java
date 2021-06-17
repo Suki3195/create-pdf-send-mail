d7846353389bf198936f3d41b0c4e5cd351c12bd
Suki3195 <sukirtisingh3195@gmail.com>
2021-06-17 11:51:32 +0530

    initial framework

---
+++ service/src/main/java/com/pdfGenertaor/PdfGenertaorApplication.java
@@ -0,0 +1,13 @@
+package com.pdfGenertaor.pdfGenertaor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfGenertaorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfGenertaorApplication.class, args);
+	}
+
+}
---
+++ service/src/test/java/com/pdfGenertaor/pdfGenertaor/PdfGenertaorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdfGenertaor.pdfGenertaor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfGenertaorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
